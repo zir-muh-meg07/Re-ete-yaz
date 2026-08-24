63beeb85bc56cbc0a14da750ea07c2d5ffc98314
zir-muh-meg07 <jvfz8t25km@privaterelay.appleid.com>
2026-08-24 18:53:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('recete-v3').then(c=>c.addAll(['./','./index.html','./manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
