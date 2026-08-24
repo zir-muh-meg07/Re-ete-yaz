d99d6b43b9a00e02981ca8d4c587e0ce4c34803f
zir-muh-meg07 <jvfz8t25km@privaterelay.appleid.com>
2026-08-24 22:28:44 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open('recete-v6').then(c=>c.addAll(['./','./index.html','./manifest.json'])))});self.addEventListener('activate',e=>{e.waitUntil(clients.claim())});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
